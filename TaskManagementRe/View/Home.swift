448669590081eff86dae5d0183ad6b8121697caa
Nuha Mohamed <nuhasafia123@gmail.com>
2023-08-03 20:16:39 -0700

    week day name not working

--- TaskManagementRe/View/Home.swift
+++ TaskManagementRe/View/Home.swift
@@ -8,35 +8,90 @@
 import SwiftUI
 
 struct Home: View {
+    @StateObject var taskModel: TaskViewModel = TaskViewModel()
     var body: some View {
         
         ScrollView(.vertical, showsIndicators: false) {
             
-            //mark: lazy stacj wuth pinned header
+            //MARK: lazy stack with pinned header
             LazyVStack(spacing: 15, pinnedViews: [.sectionHeaders])  {
                 
                 Section {
-                } header: {
                     
+                    //MARK: Current Week View
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        
+                        HStack(spacing: 10){
+                            
+                            ForEach(taskModel.currentWeek,id: \.self ){day in
+                                
+                               // EEE will return day as MON, TUE, ...
+                                
+                                Text(taskModel.extractDate(date: day, format: "EEE"))
+                            }
+                        }
+                    }
+                    
+                } header: {
+                    HeaderView()
                 }
         }
     }
 }
-    // mark: header
+    // MARK: header
     func HeaderView()->some View{
         
         HStack(spacing:10){
             
-            Text(Date().formatted(date: . abbreviated, time: .omitted))
-                .foregroundColor(.gray)
-            Text("Today")
+            VStack(alignment: .leading, spacing: 10) {
+                
+                Text(Date().formatted(date: . abbreviated, time: .omitted))
+                    .foregroundColor(.gray)
+                Text("Today")
+                    .font(.largeTitle.bold())
+            }
+            .hLeading()
+            
+            Button{
+                
+            } label: {
+                
+                Image("profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 45, height: 45)
+                    .clipShape(Circle())
+            }
         }
+        .padding()
+        .background(Color.white)
         
+        }
     }
-}
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
     }
+}
+
+// MARK: UI design Helper functions
+extension View{
+    
+    func hLeading()->some View{
+        self
+            .frame(maxWidth: .infinity,alignment: .leading)
+    }
+    
+    func hTrailing()->some View{
+        self
+            .frame(maxWidth: .infinity,alignment: .leading)
+    }
+    
+    func hCenter()->some View{
+        self
+            .frame(maxWidth: .infinity,alignment: .center)
+    }
+    
+   
 }
--- TaskManagementRe/ViewModel/TaskViewModel.swift
+++ TaskManagementRe/ViewModel/TaskViewModel.swift
@@ -33,4 +33,41 @@ struct TaskViewModel_Previews: PreviewProvider {
         
         
     ]
+    
+    //MARK: current week days
+    @Published var currentWeek: [Date] = []
+    
+    //MARK: Inititializing
+    init(){
+        func fetchCurrentWeek(){
+        }
+    }
+
+        func fetchCurrentWeek() {
+        
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let week = calendar.dateInterval(of: .weekOfMonth, for: today)
+        
+        guard let firstWeekDay = week?.start else{
+            return
+        }
+        
+        (1...7).forEach { day in
+            if let weekday = calendar.date(byAdding: .day, value: day, to: firstWeekDay) {
+                currentWeek.append(weekday)
+                }
+            }
+        }
+        
+        //MARK: Extracting Date
+    
+    func extractDate(date: Date,format: String) -> String {
+            let formatter = DateFormatter()
+            
+            formatter.dateFormat = format
+            
+            return formatter.string(from: date)
+    }
 }
