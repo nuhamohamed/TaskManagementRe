8770e944aaddd237ccdfd77261c2cf3e1896087f
Nuha Mohamed <nuhasafia123@gmail.com>
2023-08-03 18:07:02 -0700

    Initial Commit

---
+++ TaskManagementRe/TaskManagementReApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementReApp.swift
+//  TaskManagementRe
+//
+//  Created by Nuha Mohamed on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementReApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
